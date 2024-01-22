8852a91fcd5ce2056f08e8cbabd3a4e84ae7bc00
Benton Le <ble21@my.centennialcollege.ca>
2024-01-22 16:27:12 -0500

    first app

---
+++ MyCoreApp/Pages/Privacy.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System.Globalization;
+
+namespace MyCoreApp.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            string dateTime = System.DateTime.Now.ToString("d", new CultureInfo("en-US"));
+            ViewData["TimeStamp"] = dateTime;
+        }
+    }
+}
